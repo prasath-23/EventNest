8cd9fd4c2b00bb311f7d226caa133de43fafad18
Prasath <100816296+prasath-23@users.noreply.github.com>
2024-02-24 07:41:57 +0530

    Add files via upload

---
+++ Frontend/Day-3/event-manager/src/components/Constants/index.js
@@ -0,0 +1,103 @@
+import { facebook, instagram, linkedin, twitter } from '../../assets' ;
+
+export const navLinks = [
+  {
+    id: "/#home",
+    title: "Home",
+  },
+  {
+    id: "/#features",
+    title: "Features",
+  },
+  {
+    id: "/#host",
+    title: "Host",
+  },
+  {
+    id: "/#calender",
+    title: "Calender",
+  },
+  {
+    id: "/profile",
+    title: "Profile",
+  },
+];
+
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+      },
+      {
+        name: "How it Works",
+      },
+      {
+        name: "Create",
+      },
+      {
+        name: "Explore",
+      },
+      {
+        name: "Terms & Services",
+      },
+    ],
+  },
+
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+      },
+      {
+        name: "Partners",
+      },
+      {
+        name: "Suggestions",
+      },
+      {
+        name: "Blog",
+      },
+      {
+        name: "Newsletters",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+      },
+      {
+        name: "Become a Partner",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
