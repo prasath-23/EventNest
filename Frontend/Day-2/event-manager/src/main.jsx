f0065a69749c0401fd0b9519779958c144131613
Prasath <100816296+prasath-23@users.noreply.github.com>
2024-02-24 07:43:36 +0530

    Add files via upload

---
+++ Frontend/Day-2/event-manager/src/main.jsx
@@ -0,0 +1,9 @@
+import React ,{ lazy }from 'react';
+import ReactDOM from 'react-dom/client';
+import './output.css';
+import App from './App';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
